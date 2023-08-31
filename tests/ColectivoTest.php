3a8224ae05bccc4ec7a2bc13d170f1f2f4c7ef27
BautiRenaudo9-12 <renaudobautista@gmail.com>
2023-08-31 17:50:55 -0300

    delete comments

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -12,23 +12,23 @@ class ColectivoTest extends TestCase
 {
     public function testPagarConSaldoSuficiente()
     {
-        $tarjeta = new Tarjeta(150); // Creamos una tarjeta con saldo de 150
+        $tarjeta = new Tarjeta(150);
         $colectivo = new Colectivo();
 
         $boleto = $colectivo->pagarCon($tarjeta);
 
-        $this->assertInstanceOf(Boleto::class, $boleto); // Verificamos que se haya generado un boleto
-        $this->assertEquals(120, $boleto->getTarifa()); // Verificamos el monto del boleto
-        $this->assertEquals(30, $boleto->getSaldoRestante()); // Verificamos el saldo restante en la tarjeta
-        $this->assertEquals(30, $tarjeta->getSaldo()); // Verificamos que el saldo de la tarjeta se haya actualizado
+        $this->assertInstanceOf(Boleto::class, $boleto);
+        $this->assertEquals(120, $boleto->getTarifa());
+        $this->assertEquals(30, $boleto->getSaldoRestante());
+        $this->assertEquals(30, $tarjeta->getSaldo());
     }
 
     public function testPagarConSaldoInsuficiente()
     {
-        $tarjeta = new Tarjeta(100); // Creamos una tarjeta con saldo de 100
+        $tarjeta = new Tarjeta(100);
         $colectivo = new Colectivo();
 
-        $this->expectException(Exception::class); // Esperamos una excepción del tipo Exception
+        $this->expectException(Exception::class);
         $colectivo->pagarCon($tarjeta);
     }
 
