dfe68ce868ca451abe68cfa2f30999a95c4a6a1b
francobramucci <bramuccifranco1@gmail.com>
2023-09-05 00:14:33 -0300

    Agrego dataProvider y setUp en TarjetaTest.php. Testeo de cargar dinero en tarjeta

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -2,29 +2,33 @@
 namespace TrabajoSube;
 
 function chequeoCarga($monto){
+    $flag = false;
     $cargas = [150, 200, 250, 300, 350, 400, 450, 500, 
            600, 700, 800, 900, 1000, 1100, 1200, 1300, 
            1400, 1500, 2000, 2500, 3000, 3500, 4000];
-    for ($i=0; $i < sizeof($cargas) ; $i++) { 
-        if($monto != $cargas[$i]) return false;
+           
+    for ($i=0; $i <= sizeof($cargas) ; $i++) { 
+        if($monto == $cargas[$i]) $flag = true;
     }
-    return true;
+    return $flag;
 }
 
 class Tarjeta{
     public $saldo;
-
+    
     public function __construct(){
         $this->saldo = 0;
     }
 
     public function pagarBoleto($costo){
-        $this->saldo -= $costo;
+       return $this->saldo -= $costo;
     }
 
     public function cargarDinero($monto){
         $nuevosaldo = $this->saldo + $monto;
         if(chequeoCarga($monto) && $nuevosaldo <= 6600)
-            $this->saldo = $nuevosaldo;
+            return $this->saldo + $monto;
+        else 
+            return -1;
     }
 }
--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -4,16 +4,44 @@ namespace TrabajoSube;
 
 use PHPUnit\Framework\TestCase;
 
+
 class TarjetaTest extends TestCase {
-    public function testPagarBoleto() {
-        $tarjeta = new Tarjeta();
 
-        $saldo = $tarjeta->saldo;
+    public $tarjeta = null;
 
-        $costoBoleto = 120;
-
-        $tarjeta->pagarBoleto($costoBoleto);
-
-        $this->assertEquals($saldo-$costoBoleto, $tarjeta->saldo);
+    public function setUp(): void
+    {
+        $this->tarjeta = new Tarjeta();
     }
+
+    /**
+     * @dataProvider tarjetaProvider
+     */
+
+    public function testPagarBoleto($saldoini, $carga, $expected) 
+    {
+        $this->tarjeta->saldo = $saldoini;
+        $costoBoleto = 120;
+        $this->assertEquals($saldoini-$costoBoleto, $this->tarjeta->pagarBoleto($costoBoleto));
+    }
+
+    /**
+     * @dataProvider tarjetaProvider
+     */
+    public function testCargarDinero($saldo, $carga, $expected)
+    {
+        $this->tarjeta->saldo = $saldo;
+        $this->assertEquals($this->tarjeta->cargarDinero($carga), $expected);
+    }
+    
+    public static function tarjetaProvider(){
+        return [
+            [1000, 4000, 5000],
+            [5000, 2000, -1],
+            [6450, 150, 6600],
+            'Caso Limite' => [6600, 150, -1]
+        ];
+    }
+
 }
+?>
