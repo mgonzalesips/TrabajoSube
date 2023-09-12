39ff7988797238b589c2f7778f6167e3f84faf04
Facundo Campodonico <facundocampodonico04@gmail.com>
2023-09-12 08:22:28 -0300

    desarme la funcion de boleto que devolvia toda la info en 3 funciones diferentes

--- src/Boleto.php
+++ src/Boleto.php
@@ -12,11 +12,23 @@ class Boleto {
         $this->fecha = date("Y-m-d H:i:s");
     }
 
-    public function obtenerInfo() {
+    public function obtenerColectivo(){
+        return $this->colectivo;
+    }
+
+    public function obtenerTarjeta(){
+        return $this->tarjeta;
+    }
+
+    public function obtenerFecha(){
+        return $this->fecha;
+    }
+
+    /*public function obtenerInfo() {
         return "Fecha: " . $this->fecha . "\n" .
                "Colectivo: Línea " . $this->colectivo->getLinea() . ", Empresa: " . $this->colectivo->getEmpresa() . "\n" .
                "Monto pagado: $" . $this->tarjeta->getMontoPagado();
-    }
+    }*/
 }
 ?>
 
