37e6083ed0798416fc8df6a5ef526e0b34bfc093
Facundo Campodonico <facundocampodonico04@gmail.com>
2023-09-12 09:36:49 -0300

    Cubriendo todos los tests

--- tests/TarjetaViajePlusTest.php
+++ tests/TarjetaViajePlusTest.php
@@ -5,32 +5,35 @@ use PHPUnit\Framework\TestCase;
 
 class TarjetaViajePlusTest extends TestCase {
     public function testDosViajesPlus() {
-        //creo una tarjeta nueva(comienza con el saldo en 0, es decir va a tener dos viajes plus)
+        // Creo una tarjeta nueva (comienza con el saldo en 0, es decir, va a tener dos viajes plus)
         $tarjeta = new Tarjeta();
 
-        //pago 2 pasajes con saldo en 0 y verifico que ambos se pagan con el viaje plus
-
+        // Pago 2 pasajes con saldo en 0 y verifico que ambos se pagan con el viaje plus
         $tarjeta->pagarPasaje();
         $this->assertEquals($tarjeta->getSaldo(), -120);
         $tarjeta->pagarPasaje();
         $this->assertEquals($tarjeta->getSaldo(), -240);
-        //agote los saldos plus que tenia dispolibles (2)
-        
-        //intento pagar el tercero, ya con dos viajes plus en negativo y verifico que no me deja 
+        // Agoté los saldos plus que tenía disponibles (2)
 
+        // Intento pagar el tercero, ya con dos viajes plus en negativo y verifico que no me deja
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage("Saldo insuficiente para realizar un viaje plus.");
-
         $tarjeta->realizarViajePlus();
     }
-    /*
+
     public function testDescuentoViajePlus() {
-        $tarjeta = new TarjetaViajePlus(300);
+        $tarjeta = new Tarjeta();
 
-        $tarjeta->realizarViaje();
-        $saldoDespuesViaje = $tarjeta->getSaldo();
+        // Realizamos un viaje plus
+        $tarjeta->realizarViajePlus();
 
-        // Validamos que el saldo se descuente correctamente luego de otorgar el viaje plus.
-        $this->assertEquals(88.16, $saldoDespuesViaje, '', 0.01); // Usamos una tolerancia de 0.01 debido a posibles errores de redondeo
-    }*/
+        // Verificamos que el saldo después del viaje plus sea correcto
+        $this->assertEquals($tarjeta->getSaldo(), -120); // El saldo debe ser -120 después del viaje plus
+
+        // Realizamos un segundo viaje plus
+        $tarjeta->realizarViajePlus();
+
+        // Verificamos que el saldo después del segundo viaje plus sea correcto
+        $this->assertEquals($tarjeta->getSaldo(), -240); // El saldo debe ser -240 después del segundo viaje plus
+    }
 }
