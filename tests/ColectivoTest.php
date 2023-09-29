612b2e66022ea2140268d9d4aaade2e2ef3e043a
Franco Bani <131560948+franbani@users.noreply.github.com>
2023-09-29 17:02:13 -0300

    Update ColectivoTest.php

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class ColectivoTest extends TestCase{
 
     public function testpagarCon(){
-        $cole = new Colectivo()
+        $cole = new Colectivo();
         $tarj = new Tarjeta(200);
         $this->assertEquals($cole->pagarCon(tarj),80);
     }
