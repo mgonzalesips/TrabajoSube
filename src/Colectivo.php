1c66e2ee2f581664232f73ed6a7a102ad559f4ce
Franco Bani <131560948+franbani@users.noreply.github.com>
2023-09-29 14:28:21 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -7,10 +7,10 @@ class Colectivo{
     private $saldoMin = 0;
     private $costePasaje = 120;
 
-    $tarjeta1 = new Tarjeta(200)
+    $tarjeta1 = new Tarjeta(200);
     
     public function pagarCon($tarjeta){
-        if (($tarjeta->saldo - $costePasaje) >= 0){
+        if (($tarjeta->saldo - $this->costePasaje) >= 0){
             $tarjeta->saldo = $tarjeta->saldo - $costePasaje;
             $boleto->generarBoleto($tarjeta);
         }
