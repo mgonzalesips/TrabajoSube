0c4a4efb6b42e1dde0030ca06f436bd8722022af
Franco Bani <131560948+franbani@users.noreply.github.com>
2023-09-29 15:28:53 -0300

    Update ColectivoTest.php

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -6,8 +6,5 @@ use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase{
 
-    public function testGetlinea(){
-        $cole = new Colectivo(103);
-        $this->assertEquals($cole->getLinea(), 103);
     }
 }
