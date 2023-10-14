80f4187d6eaa27fc532a9bfc7e9790ca400045ea
francobramucci <bramuccifranco1@gmail.com>
2023-10-14 16:08:05 -0300

    Creo variables y métodos para limitar la cantidad y el tiempo en que se usan los boletos de la FranquiciaParcial

--- src/FranquiciaParcial.php
+++ src/FranquiciaParcial.php
@@ -6,5 +6,20 @@ class FranquiciaParcial extends Tarjeta{
     
     public function __construct(){
         Tarjeta::__construct();
+        $this->ultimopago = 0;
+        $this->cantboletos = 4;
+        $this->dia = date("d", time());
     }
+    
+    public function renovarBoletos(){
+        if($this->dia != date("d", time())){
+            $this->cantboletos = 4;
+        }
+    }
+    public $dia;
+    public $ultimopago;
+    public $cantboletos;
+
+
+
 }
