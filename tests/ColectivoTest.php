78d5c944db7bc0d89536eaea129b46100cf7c70a
francobramucci <bramuccifranco1@gmail.com>
2023-10-16 03:45:20 -0300

    Implemento funcionamiento con TiempoFalso pero arroja errores

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -7,18 +7,30 @@ use PHPUnit\Framework\TestCase;
 class ColectivoTest extends TestCase{
 
     public function testConstruct(){
-        $cole = new Colectivo(103);
+        $cole = new Colectivo(103, new Tiempo());
         $this->assertEquals($cole->linea, 103);
     }
     public function testGetlinea(){
-        $cole = new Colectivo(103);
+        $cole = new Colectivo(103, new Tiempo());
         $this->assertEquals($cole->getLinea(), 103);
     }
 
     public function testPagarCon(){
         $tarjeta = new Tarjeta();
-        $cole = new Colectivo(102);
-        $this->assertInstanceOf(Boleto::class, $cole->pagarCon($tarjeta));   
+        $cole = new Colectivo(102, new Tiempo());
+        $this->assertInstanceOf(Boleto::class, $cole->pagarCon($tarjeta, false));   
     }
-    
+
+    public function testPagarConMedio(){
+        $tarjeta = new FranquiciaParcial();
+        $tiempo = new TiempoFalso();
+        $cole = new Colectivo(102, $tiempo);
+
+        $tarjeta->cargarDinero(400);
+        $cole->pagarCon($tarjeta, true);
+        $this->assertEquals($tarjeta->saldo, 400 - $cole->costo/2);
+        $cole->tiempo->avanzar(50);
+        $cole->pagarCon($tarjeta, true);
+        $this->assertEquals($tarjeta->saldo, 400 - $cole->costo/2);
+    }
 }
